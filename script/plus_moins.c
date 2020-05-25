2f3bce1caf04891a75ff571f0532c92b1752b36c
wdazou <rarivoharivonyzoe@gmail.com>
2020-05-25 05:55:28 -0400

    mode de jeu

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -3,16 +3,16 @@
 #include <time.h>
 
 int plus_moins(int choix_joueur, int nb_atrouver)
-{ 
+{
 
 	if (choix_joueur > nb_atrouver){
 
-		printf("c'est Moins \n"); 
+		printf("Moins \n"); 
 		return 0;
 	}
 	else if (choix_joueur < nb_atrouver) {
 
-		printf("c'est Plus \n");
+		printf("Plus \n");
 		return 0;
 	}
 	else {
@@ -24,15 +24,70 @@ int plus_moins(int choix_joueur, int nb_atrouver)
 
 }
 
-int main()
+
+int main(int argc, char** argv)
 {
-	srand (time(NULL));
-	int nb_atrouver = rand() % 100;
-	int choix_joueur;
-	printf("Allez commencer a devinez: ");
-	do
-	{
-	    scanf("%d", &choix_joueur);
-		plus_moins(choix_joueur, nb_atrouver);
-	}while (choix_joueur != nb_atrouver);
+	int mode;
+	printf("\t*****************MENU PRINCIPALE*****************\n");
+        printf("Choisissez le mode de difficulte : \n1)facile\n2)moyen\n3)difficile\nVotre choix : ");
+ 	scanf("%d", &mode);
+        printf("************************************************\n");
+    
+    	srand (time(NULL));
+        int valeur_a_trouver = rand() % 100 + 1;
+    
+        int choix_joueur;
+    
+        printf("Devinez le chiffre : ");
+        int i = 0;
+    
+        switch(mode)
+        {
+    	case 1:
+    		do
+    		{
+    			scanf("%d", &choix_joueur);
+    			plus_moins(choix_joueur, valeur_a_trouver);
+    	    	}while (choix_joueur != valeur_a_trouver);
+    		break;
+    
+       case 2:
+    		do
+    		    {
+    			i++;
+    			scanf("%d", &choix_joueur);
+    	                 plus_moins(choix_joueur, valeur_a_trouver);
+    
+    		 if(i == 25)
+    		{
+    			printf("Vous avez perdu! Le chiffre est %d\n", valeur_a_trouver);
+    			break;
+    		}
+    		}while (choix_joueur != valeur_a_trouver);
+    		break;
+    
+      case 3:
+    		    	do
+    		    	{
+    			    	i++;
+    			    	scanf("%d", &choix_joueur);
+    	                            plus_moins(choix_joueur, valeur_a_trouver);
+    
+    				    if(i == 3)
+    				    {
+    					    printf("Plus de cartouche! Le chiffre est %d\n", valeur_a_trouver);
+    					    break;
+    				}
+    			    }while (choix_joueur != valeur_a_trouver);
+    	                break;
+    
+    		    default:
+    			    printf("Erreur! Vous deviez choisir entre 1, 2 et 3.\n");
+    			    break;
+    	    }
 }
+
+    	    
+
+
+
